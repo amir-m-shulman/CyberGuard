7e9932a9706f721cdd0c98e0534d0fe4ae02a338
amir-m-shulman <amir.shulman08@gmail.com>
2024-03-09 13:46:02 +0200

    yey

--- CyberGuard/CyberGuard/Assets/script/viruses/kill_by_enter.cs
+++ CyberGuard/CyberGuard/Assets/script/viruses/kill_by_enter.cs
@@ -6,10 +6,18 @@ public class kill_by_enter : MonoBehaviour
 {
     [SerializeField] int enters_num;
     [SerializeField] GameObject p;
+    [SerializeField] GameObject[] destroy_together;
     // Start is called before the first frame update
     void Start()
     {
-        if(spawn_enemy.enters == enters_num) { Destroy(gameObject); p.transform.position = transform.position; }
+        if(spawn_enemy.enters == enters_num) 
+        {
+            foreach (GameObject c in destroy_together)
+            {
+                Destroy(c);
+            }
+            p.transform.position = transform.position; 
+        }
     }
 
     // Update is called once per frame
